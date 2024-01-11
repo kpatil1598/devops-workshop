67f71cd0b4a66c68e8c901d6986938b29faa9b14
kpatil <ketanpatil51598@gmail.com>
2024-01-11 18:27:38 +0530

    new terraform files added

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,11 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIARY7CNIPSRAGXSRSP"
+  secret_key = "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"
 }
 
-resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+resource "aws_instance" "web" {
+    ami = "ami-0005e0cfe09cc9050"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "devops_project"
 }
--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -1,36 +1,29 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIARY7CNIPSRAGXSRSP"
+  secret_key = "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"
 }
 
-resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+resource "aws_instance" "demo_server" {
+    ami = "ami-0005e0cfe09cc9050"
     instance_type = "t2.micro"
-    key_name = "dpp"
-    security_groups = [ "demo-sg" ]
+    key_name = "devops_project"
+    security_groups = [ demo_sg ]
 }
 
-resource "aws_security_group" "demo-sg" {
-  name        = "demo-sg"
-  description = "SSH Access"
-  
+resource "aws_security_group" "demo_sg" {
+  name        = "demo_sg"
+  description = "ssh access"
+
   ingress {
-    description      = "Shh access"
+    description      = "ssh access"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = [0.0.0.0/0]
   }
 
   tags = {
-    Name = "ssh-prot"
-
+    Name = "ssh=access"
   }
 }
