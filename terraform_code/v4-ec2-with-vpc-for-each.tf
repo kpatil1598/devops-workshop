9a7cb3fb8e89b67c6ea7965a0c8a9a4b08174389
kpatil1598 <147227225+kpatil1598@users.noreply.github.com>
2024-02-09 10:37:22 +0530

    Update v4-ec2-with-vpc-for-each.tf
    added ingress rule for jenkins

--- terraform_code/v4-ec2-with-vpc-for-each.tf
+++ terraform_code/v4-ec2-with-vpc-for-each.tf
@@ -29,6 +29,15 @@ resource "aws_security_group" "demo_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+   ingress {
+     description      = "jenkins access"
+     from_port        = 8080
+     to_port          = 8080
+     protocol         = "tcp"
+     cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
    egress {
      description      = "outgoing trafic"
      from_port        = 0
